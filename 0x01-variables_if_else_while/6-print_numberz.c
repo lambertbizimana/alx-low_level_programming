de0363b772bf09c10bd53f3ffa58c35d3ca30a81
lambertbizimana <bizilambert1994@gmail.com>
2022-07-05 14:16:52 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+/**
+ *
+ *Main used to executes the block of codes
+ *
+ *
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+
+
+int a;
+
+for (a = 0; a < 10 ; a++)
+{
+
+
+putchar(a + '0');
+
+
+}
+putchar('\n');
+return 0;
+
+
+
+}
